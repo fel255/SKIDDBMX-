e25867720cfd96f36269d5a39a5d13d5b515bd65
SKIDDYBMA <felician627@gmail.com>
2025-06-10 13:52:19 +0300

    ytdl-play.js

--- plugins/ytdl-play.js
+++ plugins/ytdl-play.js
@@ -69,7 +69,7 @@ cmd({
             ptt: false
         }, { quoted: mek });
 
-        await reply(`✅ *${title}* 『 ⚡𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝕾𝖎𝖗 𝕷𝖔𝖋𝖙⚡ 』`);
+        await reply(`✅ *${title}* 『 ⚡𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 ℑ𝔗-𝔗𝔈ℭℌ-𝔅ℜ𝔒-𝔖⚡ 』`);
 
     } catch (error) {
         console.error('Handler error:', error);
